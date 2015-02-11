1af242003834f3820ccf4153d59e4f1e4be0d2c3
M0unir <mounir.chouladi@gmail.com>
2015-02-11 11:58:48 -0800

    Added Genre model and API get request for genres

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-var express =  require("express");
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
-var mongoose =  require("mongoose");
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Models
+Genre = require('./models/genre');
 
 // Connecting to mongoose
-
 mongoose.connect('mongodb://localhost/booklibrary');
 var db = mongoose.connection;
 
-app.get('/', function(req,res){
-    res.send('Hello World');
+app.get('/', function (req, res) {
+  res.send('Hello World');
 });
 
-app.get('/about', function(req,res){
-    res.send('<h1>About page</h1>');
-    
+app.get('/about', function (req, res) {
+  res.send('<h1>About page</h1>');
+
 });
 
-app.get('/contact', function(req,res){
-    res.send('Contact Page');
+app.get('/contact', function (req, res) {
+  res.send('Contact Page');
 });
+
+// Book API
+app.get('/api/genres', function(req,res){
+    Genre.getGenres(function(err, genres){
+        if(err){
+            throw err;
+        }
+        res.json(genres);
+    })
+})
 
 app.listen(3000);
 console.log('Running on port 3000..');
