d547759a68e162a96ce67281215ba9b5f5aca899
M0unir <mounir.chouladi@gmail.com>
2015-02-16 17:21:12 -0800

    Added books and book detail view

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -2,12 +2,18 @@ var bookstore = angular.module('bookstore');
 
 
 bookstore.controller('BooksController', function ($scope, $http, $location, $routeParams) {
-    console.log('BooksController loaded... ');
-    $scope.getBooks = function(){
-        $http.get('api/books').success(function(response){
-            $scope.books = response;
-        })
-    }
+  console.log('BooksController loaded... ');
+  $scope.getBooks = function () {
+    $http.get('api/books').success(function (response) {
+      $scope.books = response;
+    });
+  }
+
+  $scope.getBook = function () {
+      var id = $routeParams.id;
+    $http.get('api/books/'+id).success(function (response) {
+      $scope.book = response;
+    });
+  }
 
 });
-
